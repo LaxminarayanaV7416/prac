5ff6856367ab286561894d8e5b2af72a53078876
LaxminarayanaV7416 <laxminarayana.vadnala@gmail.com>
2021-02-15 00:51:46 +0530

    with succesful integrationof opencv

--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import inject from './injectOpenCV';
+import { OpenCvConsumer } from 'opencv-react';
+const OPENCV_URL = 'https://docs.opencv.org/3.4.0/opencv.js';
+
+const promise = inject('opencv',OPENCV_URL);
+
+promise.then(() => {
+      console.log(`success to load ${OPENCV_URL}`);
+      // eslint-disable-next-line no-undef
+      const opencv = cv;
+      ReactDOM.render(
+        <React.StrictMode>
+          <App module={opencv}/>
+        </React.StrictMode>,
+        document.getElementById('root')
+      );
+    })
+    .catch(() => {
+      // eslint-disable-next-line no-console
+      console.log(`Failed to load ${OPENCV_URL}`);
+    });
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
